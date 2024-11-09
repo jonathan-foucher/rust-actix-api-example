b86547f9632d706fcf7d4daca9c395a4f28b97c0
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-09 01:58:26 +0100

    use config file for env variables

--- src/routers/http_server.rs
+++ src/routers/http_server.rs
@@ -2,8 +2,15 @@ use actix_web::{web, App, HttpServer};
 
 use crate::routers::movie_router::{get_all_movies, save_movie, delete_movie};
 
+const DEFAULT_PORT: u16 = 8080;
+
 #[actix_web::main]
 pub async fn start_server() -> std::io::Result<()> {
+    let http_port: u16 = std::env::var("HTTP_PORT")
+        .map(|http_port| http_port.parse::<u16>().unwrap_or(DEFAULT_PORT))
+        .unwrap_or(DEFAULT_PORT);
+    println!("Application is starting on port {}", http_port);
+
     HttpServer::new(|| {
         App::new()
             .service(web::scope("/api")
@@ -12,7 +19,7 @@ pub async fn start_server() -> std::io::Result<()> {
                 .service(delete_movie)
             )
     })
-    .bind(("127.0.0.1", 3000))?
+    .bind(("127.0.0.1", http_port))?
     .run()
     .await
 }
