dbcaedb88dfd9ad4253f481fc6225df8114f3b89
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-10 01:07:57 +0100

    set max pool connections

--- src/database/database_connection.rs
+++ src/database/database_connection.rs
@@ -1,12 +1,15 @@
 use diesel::{pg::PgConnection, r2d2::{ConnectionManager, Pool}};
 use std::env;
 
+const MAX_POOL_SIZE: u32 = 2;
+
 pub fn create_db_pool() -> Pool<ConnectionManager<PgConnection>> {
     println!("Connecting to the database");
     let database_url = env::var("DB_URL").expect("DB_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
 
     Pool::builder()
+            .max_size(MAX_POOL_SIZE)
             .test_on_check_out(true)
             .build(manager)
             .expect("Could not build connection pool")
